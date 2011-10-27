543819492183ce8a1d77200f0d3788875b0be7cd
RayHightower <rth@wisdomgroup.com>
2011-10-27 10:30:46 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Branchsandbox::Application.config.secret_token = '414484c930578d6e90fa5c9a98cfd261eb4cee7048e330eda2367e6ebd4256fd6d9d8911febfaa6d71f831cafd497613de5982b9dc3517826930a2f73bb80e18'
